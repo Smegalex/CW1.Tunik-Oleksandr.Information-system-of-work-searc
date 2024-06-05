1024122191614bb3c94eff3dc4c52b213eeb8ea0
Smegalex <smegalsuper@gmail.com>
2024-06-05 14:46:31 +0300

    Init

---
+++ src/main/java/com/igor_sikorsky_ist_hub/tunik_oleksandr/cw1_information_system_of_work_search/resources/JakartaEE10Resource.java
@@ -0,0 +1,20 @@
+package com.igor_sikorsky_ist_hub.tunik_oleksandr.cw1_information_system_of_work_search.resources;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.core.Response;
+
+/**
+ *
+ * @author 
+ */
+@Path("jakartaee10")
+public class JakartaEE10Resource {
+    
+    @GET
+    public Response ping(){
+        return Response
+                .ok("ping Jakarta EE")
+                .build();
+    }
+}
---
+++ src/main/webapp/WEB-INF/jsp/employer/create.jsp
@@ -0,0 +1,17 @@
+<%-- 
+    Document   : create
+    Created on : 5 черв. 2024 р., 14:46:19
+    Author     : sasha
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <h1>Hello World!</h1>
+    </body>
+</html>
